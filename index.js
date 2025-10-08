cc1f93d9d72984c1e1ef53cf55f66d206c4f56f5
Alfr3d <armageddion@gmail.com>
2025-10-08 10:19:02 +0200

    add new stream link for trial project

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function checkPassword(event) {
     event.preventDefault();
     const password = prompt("Enter password to continue:");
     if (password === "museum2025") {
-        window.location.href = "https://share.arcware.cloud/v1/share-658d3f13-6094-4e1c-8ab0-063aac84372a";
+        window.location.href = "https://share.arcware.cloud/v1/share-736d9389-fc24-4007-888a-5241e9ec8e56";
     } else if (password !== null) {
         alert("Incorrect password.");
     }
